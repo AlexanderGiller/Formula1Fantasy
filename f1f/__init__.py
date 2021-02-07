367dc64e2e7386baa972698d4e73e79426c5c3a2
Jonas Buse <jonastbuse@gmail.com>
2021-02-07 19:22:47 +0100

    modified .gitignore ...to stop tracking the db file and some minor refactoring

--- f1f/__init__.py
+++ f1f/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = b'\xef\xec=\x17\x10\xab|h\xadZ\xd1\t\xf8\x13\xec\xe5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import sys
 from f1f import app
 
-if sys.platform.lower() == "win32": 
+if sys.platform.lower() == "win32":
     os.system('color')
 
 if __name__ == '__main__':
