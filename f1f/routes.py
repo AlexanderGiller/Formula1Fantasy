ee5755f90e22c8d74d78896a2f62ff573e38273d
Jonas Buse <jonastbuse@gmail.com>
2021-02-07 19:28:37 +0100

    registration bug fixed

--- f1f/routes.py
+++ f1f/routes.py
@@ -53,7 +53,7 @@ def register():
 
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_pw = bcrypt.generate_password_hash(form.password.data)
         user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
         db.session.add(user)
         db.session.commit()
